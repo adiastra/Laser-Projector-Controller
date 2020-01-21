356c2585082f6f4a856c9095f324d504561fb60c
ADiAstra <alecdiastra@gmail.com>
2020-01-20 18:06:56 -0700

    Added comments and spelling corrections

--- laser_control.ino
+++ laser_control.ino
@@ -1,12 +1,12 @@
 #define VERSION "Version 1.3"
  /*
-* This is the DiAstra Laser Coltrol
+* This is the DiAstra Laser Controler
 * It is designed to control safety and sensors for a laser projector
-* The shutter is a relay which is closed (cutting power to the lasers)
+* The ILDA shutter is a relay which is closed (cutting power to the lasers)
 * unless the interlock circuit is complete (unbroken)
 * and the shutter signal from the software/DAC is set to HIGH (+5v)
 * if either of these conditions is false the shutter remains closed
-* there is a 3 second delay after closing the shutter before you can open it again
+* there is a 5 second delay after closing the shutter before you can open it again
 * Written by: Alec DiAstra (alecdiastra@gmail.com)
  */
 
@@ -52,7 +52,7 @@ digitalWrite(interlockOut, HIGH);
     
    if (millis() > delayTime + SAFETY_DELAY) {
 
-        //The interlock is a circuit and the shutter is set to 'open'
+        //If the interlock is a circuit AMD the shutter is set to 'open'
         if( (digitalRead(interlockIn) == HIGH) && (digitalRead(shutterSignal) == HIGH) )  {             
             // Open the shutter
             digitalWrite(shutterPin, RELAY_SIGNAL);
@@ -66,7 +66,7 @@ digitalWrite(interlockOut, HIGH);
         //If interlock curcuit broken
         else if(digitalRead(interlockIn) == LOW)  {
             // Close the shutter
-            digitalWrite(shutterPin, !RELAY_SIGNAL);// set shutter pin to HIGH
+            digitalWrite(shutterPin, !RELAY_SIGNAL);
             Serial.println("Shutter Closed - Interlock Fault");
             shutterMessage = false;
             delayTime = millis();               
@@ -75,7 +75,7 @@ digitalWrite(interlockOut, HIGH);
         //Otherwie the shutter signal is 'closed'
         else  {     
             // Close the shutter
-            digitalWrite(shutterPin, !RELAY_SIGNAL);// set shutter pin to HIGH
+            digitalWrite(shutterPin, !RELAY_SIGNAL);
             Serial.println("Shutter Closed - No shutter signal");
             shutterMessage = false;
             delayTime = millis();
@@ -85,27 +85,32 @@ digitalWrite(interlockOut, HIGH);
    else {
         //Close the shutter
         digitalWrite(shutterPin, HIGH);
-        
+
+        //If the interlock circuit is broken at boot
         if ((digitalRead(interlockIn) == LOW) && (boot == true))  {
            delayTime = millis();
            Serial.println("Shutter Closed - Interlock Fault");
            boot = false;         
         }
 
+        //If the shutter signal is off at boot (which is totally acceptabl)
         if ((digitalRead(shutterSignal) == LOW) && (boot == true)){
           delayTime = millis();
-          Serial.println("Ready");
+          Serial.println("SHutter Closed - Ready");
           boot = false; 
         }
 
+        //If the interlock circuit is re-broken during the safety delay period  restart the the delay count
         if (digitalRead(interlockIn) == LOW) {
           delayTime = millis();
         }
 
+        //If the shutter is re-closed durring the delay period restart the delay count
         if (digitalRead(shutterSignal) == LOW){
           delayTime = millis();
         }
 
+        //If the last state of the laser was ON let the user know that we are in a safety delay period
         if (laserOn == true){
           Serial.println("Shutter Closed - Safety Delay");
           laserOn = false;
